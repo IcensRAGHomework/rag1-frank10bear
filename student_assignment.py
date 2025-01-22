83e52deaca30d80f4ece049aa537b0376e242d82
frank10bear <frank10bear@gmail.com>
2025-01-22 14:59:33 +0800

    push hw01, run1

--- student_assignment.py
+++ student_assignment.py
@@ -6,12 +6,42 @@ from model_configurations import get_model_configuration
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
 
+from langchain.prompts import ChatPromptTemplate
+from langchain.output_parsers import (ResponseSchema, StructuredOutputParser)
+
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
 def generate_hw01(question):
-    print("print generate_hw01")
-    pass
+    llm = AzureChatOpenAI(
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
+    )
+
+    response_schemas = [
+        ResponseSchema(
+            name="date",
+            description="該紀念日的日期",
+            type="YYYY-MM-DD"),
+        ResponseSchema(
+            name="name",
+            description="該紀念日的名稱")
+    ]
+    output_parser = StructuredOutputParser(response_schemas=response_schemas)
+    format_instructions = output_parser.get_format_instructions()
+    prompt = ChatPromptTemplate.from_messages([
+        ("system","使用台灣語言並回答問題，答案請以JSON格式的list清單,{format_instructions}"),
+        ("human","{question}")])
+    prompt = prompt.partial(format_instructions=format_instructions)
+    response = llm.invoke(prompt.format(question=question)).content
+    response = response.replace("```json", "")
+    response = response.replace("```", "")
+    response = "{ \"Result\":" + response + "}"
+    return response
     
 def generate_hw02(question):
     pass
@@ -39,3 +69,5 @@ def demo(question):
     response = llm.invoke([message])
     
     return response
+
+print(generate_hw01("2024年台灣10月紀念日有哪些?"))
